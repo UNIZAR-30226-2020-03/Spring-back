34b08589f784959ab159e3a96488fda27e276dd5
alexitoguiz <alessruizsss@gmail.com>
2020-04-08 18:38:36 +0200

    vEliminado

--- upbeat/src/main/java/com/software/upbeat/api/ArtistaResponse.java
+++
@@ -1,5 +0,0 @@
-package com.software.upbeat.api;
-
-public class ArtistaResponse {
-
-}
--- upbeat/src/main/java/com/software/upbeat/dao/ArtistaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.software.upbeat.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.software.upbeat.model.Artista;
-
-public interface ArtistaRepository extends JpaRepository<Artista, Long>{
-
-}
