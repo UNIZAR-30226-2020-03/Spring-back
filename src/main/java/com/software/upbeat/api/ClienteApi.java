0982f25e77483013782a6050046ec43f1f9a3202
Alejandro Piedrafita <757311@unizar.es>
2020-04-09 14:07:21 +0200

    Cliente

--- src/main/java/com/software/upbeat/api/ClienteApi.java
+++ src/main/java/com/software/upbeat/api/ClienteApi.java
@@ -1,22 +1,22 @@
 package com.software.upbeat.api;
 
+import java.util.List;
+
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.software.upbeat.exception.ResourceNotFoundException;
 import com.software.upbeat.model.Cliente;
 import com.software.upbeat.service.ClienteService;
 
 @RestController
 public class ClienteApi {
-	
-	@RequestMapping(value="/cliente", method=RequestMethod.GET)
-	public Cliente getById() {
-		return new Cliente(1L, "Pepe", "Ruiz", "pepecontra", "pepito@gmail.com", "pepitomusic", "España");
-	}
 	
 	@Autowired
 	ClienteService clienteService;
@@ -24,7 +24,39 @@ public class ClienteApi {
 	@Autowired
 	Mapper mapper;
 	
-	@RequestMapping(value="/cliente", method=RequestMethod.POST)
+	//////////////////////////////////////////////
+	// OBTENER TODOS LOS CLIENTES				//
+	//////////////////////////////////////////////
+	
+	@RequestMapping(value="/allClientes", method=RequestMethod.GET)
+	public List<Cliente> getAllClientes() {
+		return clienteService.getAllClientes();
+	}	
+	
+	//////////////////////////////////////////////
+	// OBTENER CLIENTE POR ID					//
+	//////////////////////////////////////////////
+	@RequestMapping(value="/cliente/{cod_cliente}", method=RequestMethod.GET)
+	public ClienteResponse getById(@PathVariable(value = "cod_cliente") Long clienteId) throws ResourceNotFoundException {
+		
+		// Mapeo request dto
+		// Cliente cliente = mapper.map(clienteRequest, Cliente.class);
+		
+		// Invoca lógica de negocio
+		ResponseEntity<Cliente> clienteById = clienteService.getClienteById(clienteId);
+				
+		// Mapeo entity
+		ClienteResponse clienteResponse = mapper.map(clienteById, ClienteResponse.class);
+				
+		return clienteResponse;
+				
+		// SE PODRÍA HACER DE FORMA MÁS BREVE PERO ASÍ SE RESALTA CADA PASO DE FORMA INDEPENDIENTE
+	}
+	
+	//////////////////////////////////////////////
+	// ACTUALIZAR O GUARDAR CLIENTE				//
+	//////////////////////////////////////////////	
+	@RequestMapping(value="/saveCliente", method=RequestMethod.POST)
 	public ClienteResponse updateOrSave(@RequestBody ClienteRequest clienteRequest) {
 		
 		// Mapeo request dto
@@ -40,4 +72,6 @@ public class ClienteApi {
 		
 		// SE PODRÍA HACER DE FORMA MÁS BREVE PERO ASÍ SE RESALTA CADA PASO DE FORMA INDEPENDIENTE
 	}
+	
 }
+
--- src/main/java/com/software/upbeat/service/ClienteService.java
+++ src/main/java/com/software/upbeat/service/ClienteService.java
@@ -1,9 +1,15 @@
 package com.software.upbeat.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.software.upbeat.dao.ClienteRepository;
+import com.software.upbeat.exception.ResourceNotFoundException;
 import com.software.upbeat.model.Cliente;
 
 @Component
@@ -14,6 +20,17 @@ public class ClienteService {
 	
 	public Cliente save(Cliente cliente){
 		return dao.saveAndFlush(cliente);
+	}
+	
+	// OBTENER TODOS LOS CLIENTES
+	public List<Cliente> getAllClientes() {
+		return this.dao.findAll();
+	}
+	
+	// OBTENER CLIENTE POR ID
+	public ResponseEntity<Cliente> getClienteById(Long id) throws ResourceNotFoundException{
+		Cliente cliente = dao.findById(id).orElseThrow(() -> new ResourceNotFoundException("No existe cliente con este id: " + id));
+		return ResponseEntity.ok().body(cliente);
 	}
 	
 }
