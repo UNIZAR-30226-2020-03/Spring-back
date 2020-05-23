48840d29b08c6996f414635cec470628a3fc31da
Alejandro Piedrafita <757311@unizar.es>
2020-05-23 15:33:47 +0200

    TODAS LAS OPERACIONES COLA
    #12

--- src/main/java/com/software/upbeat/model/ListaReproduccion.java
+++ src/main/java/com/software/upbeat/model/ListaReproduccion.java
@@ -19,6 +19,9 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Table(name="listaReproduccion")
 public class ListaReproduccion {
 	
+	private static int ACTUAL = 0;
+	private static int SIGUIENTE = 1;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
@@ -80,7 +83,7 @@ public class ListaReproduccion {
 	@ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.LAZY)
 	private List<Cancion> canciones; //= new HashSet<Cancion>();
 	
-	Iterator<Cancion> iterador = null;
+	//Iterator<Cancion> iterador = null;
 	/*
 	* https://stackoverflow.com/questions/20119142/jackson-multiple-back-reference-properties-with-name-defaultreference
 	*/
@@ -93,41 +96,48 @@ public class ListaReproduccion {
 		this.canciones = canciones;
 	}
 	
-	public void actualizar_num_canciones() {
-		this.num_canciones = canciones.size();
-	}
+	
+	//////////////////////////////////////////////////////////////
+	// OPERACIONES COLA DE REPRODUCCIÓN							//
+	//////////////////////////////////////////////////////////////
 	
 	public void actualizar_segundoReproduccion(int segundo) {
 		this.segundoReproduccion = segundo;
 	}
 	
-	public void addCancion(Cancion cancion) {
-		canciones.remove(cancion);
-		canciones.add(cancion);
-		actualizar_num_canciones();
+	public void actualizar_num_canciones() {
+		this.num_canciones = canciones.size();
 	}
 	
+	/*
+	 * QUITAR UNA CANCIÓN DE LA LISTA
+	 */
 	public void removeCancion(Cancion cancion) {
 		canciones.remove(cancion);
 		actualizar_num_canciones();
 	}
 	
+	/*
+	 * VACIAR LA LISTA
+	 */
 	public void clear() {
 		canciones.clear();
+		actualizar_num_canciones();
 		
 	}
 	
 	/*
 	 * REPRODUCIR UNA CANCIÓN
 	 * 
-	 * SI ESTÁ VACÍA LA LISTA -> cancion = null, segs = -1
-	 * SI NO -> cancion = siguiente canción del iterador, segundos = segundos actuales
+	 * SI num_canciones <=0 -> cancion = null, segs = -1
+	 * SI NO -> cancion = canción en posición 0, segundos = segundos actuales
 	 */
 	public CancionListaReproduccion play(){
 		Cancion cancion;
 		int segs;
-		if(iterador.hasNext()) {
-			cancion = iterador.next();
+		actualizar_num_canciones();
+		if(num_canciones > ACTUAL) {
+			cancion = canciones.get(ACTUAL);
 			segs = this.segundoReproduccion;
 		}
 		else {
@@ -140,37 +150,89 @@ public class ListaReproduccion {
 	/*
 	 * PAUSAR UNA CANCIÓN
 	 * 
-	 * Se actualizan los segundos 
+	 * Se actualiza el segundo en el cual se ha parado la canción
 	 */
 	public void parar(int segundoParar) {
 		this.segundoReproduccion = segundoParar;
 	}
 	
 	/*
+	 * PASAR UNA CANCIÓN
 	 * 
+	 * SI num_canciones <= 1 -> cancion = null, segs = -1
+	 * SI NO -> cancion = canción en posición 1, segundos = segundos actuales, se quita de la lista la primera canción
+	 * 
+	 * DE ESTA FORMA, LA SIGUIENTE CANCIÓN SE CONVIERTE EN LA CABEZA DE LA COLA
 	 */
-	public void reproducirCancion() {}
+	public CancionListaReproduccion next(){
+		Cancion cancion;
+		int segs;
+		actualizar_num_canciones();
+		if(num_canciones > SIGUIENTE) {
+			cancion = canciones.get(SIGUIENTE);
+			segs = this.segundoReproduccion;
+			canciones.remove(ACTUAL);
+		}
+		else {
+			cancion = null;
+			segs = -1;
+		}
+		return new CancionListaReproduccion(cancion, segs);
+	}
 	
 	/*
-	 * 
+	 * AÑADE LA CANCIÓN QUE SE QUIERE REPRODUCIR AL PRINCIPIO DE LA COLA
 	 */
-	public void reproducirLista() {}
+	public CancionListaReproduccion reproducirCancion(Cancion cancion) {
+		canciones.remove(cancion);
+		canciones.add(ACTUAL, cancion);
+		actualizar_num_canciones();
+		int segs = 0;
+		return new CancionListaReproduccion(cancion, segs);
+	}
 	
 	/*
+	 * AÑADE LA CANCIÓN QUE SE QUIERE REPRODUCIR AL PRINCIPIO DE LA COLA
 	 * 
+	 * AÑADE EL RESTO DE CANCIONES DE LA LISTA (ÁLBUM O PLAYLIST) A LA COLA,
+	 * A CONTINUACIÓN DE ESTA PRIMERA CANCIÓN
 	 */
-	public void addCancion() {}
+	public CancionListaReproduccion reproducirLista(List<Cancion> cancionList) {
+		canciones.removeAll(cancionList);
+		canciones.addAll(ACTUAL, cancionList);
+		actualizar_num_canciones();
+		Cancion cancion = canciones.get(ACTUAL);
+		int segs = 0;
+		return new CancionListaReproduccion(cancion, segs);
+	}
 	
 	/*
-	 * 
+	 * AÑADE A LA COLA UNA CANCIÓN Y ACTUALIZA EL NÚMERO DE CANCIONES
 	 */
-	public void addLista() {}
+	public void addCancion(Cancion cancion) {
+		canciones.remove(cancion);
+		canciones.add(cancion);
+		actualizar_num_canciones();
+	}
+	
+	/*
+	 * AÑADE A LA COLA UNA LISTA DE CANCIONES Y ACTUALIZA EL NÚMERO DE CANCIONES
+	 * 
+	 * UTILIZADO PARA AÑADIR PLAYLISTS O ÁLBUMES A LA COLA
+	 */
+	public void addLista(List<Cancion> cancionList) {
+		canciones.removeAll(cancionList);
+		canciones.addAll(cancionList);
+		actualizar_num_canciones();
+	}
+	//////////CANCIONES /////////////////////
 	
 	public ListaReproduccion() {
 		super();
 		this.segundoReproduccion = 0;
+		this.num_canciones = 0;
 	}
 	
-	//////////CANCIONES /////////////////////
+	
 
 }
