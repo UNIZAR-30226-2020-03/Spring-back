422caa5e6b183466cdc596984fb8480b7bf5ed06
alexitoguiz <alessruizsss@gmail.com>
2020-05-25 11:21:10 +0200

    Update UpbeatApplication.java

--- src/main/java/com/software/upbeat/UpbeatApplication.java
+++ src/main/java/com/software/upbeat/UpbeatApplication.java
@@ -2,7 +2,7 @@ package com.software.upbeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*LA GUARRAAAAAAAAA*/
 @SpringBootApplication
 public class UpbeatApplication {
 
